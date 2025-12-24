97c2b8c7580af634e09eb10622575ce8fd8afd17
powworld294 <louispow2345@gmail.com>
2025-12-24 04:14:26 +0000

    Update contact map to postcode BH5 1DR

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -344,14 +344,14 @@ export default function Contact() {
 
               <div className="relative h-80">
                 <iframe
-                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2483.540943668359!2d-0.1276!3d51.5074!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNTHCsDMwJzI2LjYiTiAwwrAwNyczOS40Ilc!5e0!3m2!1sen!2suk!4v1234567890"
+                  src="https://maps.google.com/maps?q=BH5%201DR&z=15&output=embed"
                   width="100%"
                   height="100%"
                   style={{ border: 0 }}
                   allowFullScreen
                   loading="lazy"
                   referrerPolicy="no-referrer-when-downgrade"
-                  title="Office location"
+                  title="Office location - BH5 1DR"
                 />
               </div>
             </div>
